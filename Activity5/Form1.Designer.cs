adac2fe14c88156851854c020d07f1f88e863d1e
andy1 <andy10112004@gmail.com>
2025-09-04 06:55:11 +0800

    ji

--- Activity5/Form1.Designer.cs
+++ Activity5/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(35, 40);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(715, 264);
+            listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(303, 334);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(447, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(223, 392);
+            button1.Name = "button1";
+            button1.Size = new Size(335, 34);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(126, 337);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Label label1;
     }
 }
--- Activity5/Form1.cs
+++ Activity5/Form1.cs
@@ -6,5 +6,47 @@ namespace Activity5
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("SalesInteger");
+            comboBox1.Items.Add("PriceDecimal");
+            comboBox1.Items.Add("QuantityInteger");
+            comboBox1.Items.Add("NameItemString");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int QuantityInteger;
+            decimal PriceDecimal;
+            string NameItemsString;
+            decimal SalesInteger;
+
+            QuantityInteger = 5;
+            PriceDecimal = 40.5m;
+            SalesInteger = PriceDecimal * QuantityInteger;
+            NameItemsString = "Book of Knowledge";
+
+            if (comboBox1.Text == "SalesInteger")
+            {
+                listBox1.Items.Add(comboBox1.Text + " " + SalesInteger.ToString("C"));
+            }
+            else if (comboBox1.Text == "PriceDecimal")
+            {
+                listBox1.Items.Add(comboBox1.Text + " " + PriceDecimal.ToString("C"));
+            }
+            else if (comboBox1.Text == "QuantityInteger")
+            {
+                listBox1.Items.Add(comboBox1.Text + " " + QuantityInteger.ToString());
+            }
+            else if (comboBox1.Text == "NameItemString")
+            {
+                listBox1.Items.Add(comboBox1.Text + " " + NameItemsString);
+            }
+            else
+            {
+                MessageBox.Show("Please select a variable name from the ComboBox.");
+            }
+        }
     }
 }
