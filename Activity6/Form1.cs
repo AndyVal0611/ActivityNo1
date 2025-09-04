89e6f9e31d9fab6f3966480baf850636360ecdfa
andy1 <andy10112004@gmail.com>
2025-09-04 09:46:56 +0800

    bazinga

---
+++ Activity6/Form1.cs
@@ -0,0 +1,12 @@
+namespace Activity6
+{
+    public partial class Form1 : Form
+    {
+        private double total_amount = 0;
+        private int total_qty = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
